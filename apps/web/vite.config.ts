b94f33f99007a420f3e69bdf7173d17bfb1732ed
Elivelton Santos <samsepi0l.dev@gmail.com>
2024-10-01 17:54:01 -0300

    chore: git workflows implementations

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,15 +1,7 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig(({ command }) => {
-	const config = {
-		plugins: [react()],
-		base: "/",
-	};
-
-	if (command !== "serve") {
-		config.base = "/my-vite-react-app/";
-	}
-	return config;
-});
+export default defineConfig({
+  plugins: [react()],
+})

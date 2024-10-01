435c8d3b5fe5f495a99d41bed3ad56b5bc4dac1d
Elivelton Santos <samsepi0l.dev@gmail.com>
2024-10-01 15:43:14 -0300

    chore: config server to deploy

--- apps/server/dist/env.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.env = void 0;
-const zod_1 = require("zod");
-const envSchema = zod_1.z.object({
-    DATABASE_URL: zod_1.z.string().url(),
-});
-exports.env = envSchema.parse(process.env);
